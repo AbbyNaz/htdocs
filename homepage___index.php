5f31b58dbf08834e94afe763bb1cd6c7ed76f68b
AbbyNaz <113605281+AbbyNaz@users.noreply.github.com>
2022-12-23 15:07:19 +0800

    articles and footer

--- homepage___index.php
+++ homepage___index.php
@@ -13,6 +13,7 @@ include('includes/homepage___header.php');
             <li><a href="homepage___index.php">Home</a></li>
             <!-- <li><a href="homepage___about.php">About</a></li> -->
             <li><a href="homepage___articles.php">Articles</a></li>
+            <!-- <li><a href="homepage___FAQS.php">FAQS</a></li> -->
             <!-- <li><a href="individual_inventory_form.php">Registration</a></li> -->
 
             <li><button class="login-btn"><a href="loginForm.php">Login</a></button></li>
