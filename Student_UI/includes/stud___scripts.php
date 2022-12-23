c448d4f3daf5d021357e3ddf52d34e6c8c879899
AbbyNaz <113605281+AbbyNaz@users.noreply.github.com>
2022-12-23 16:17:06 +0800

    tanggalin yung tawk chat sa scripts

--- Student_UI/includes/stud___scripts.php
+++ Student_UI/includes/stud___scripts.php
@@ -110,7 +110,7 @@
    <script src="js/main.js"></script>
    <!-- tawk chat JS
         ============================================ -->
-   <script src="js/tawk-chat.js"></script>
+   <!-- <script src="js/tawk-chat.js"></script> -->
 <!--    <script src="includes/try_calendar.js"></script> -->
 
    <!-- for the modal -->
